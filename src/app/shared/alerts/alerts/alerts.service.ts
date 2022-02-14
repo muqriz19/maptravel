c1b4064fe9c0044647bed6fe78e83d176e0b427f
muqriz19 <muqriz19othman@gmail.com>
2022-02-14 10:32:54 +0800

    added alerts component

---
+++ src/app/shared/alerts/alerts/alerts.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AlertsService {
+  constructor() {}
+
+  public displayAlerts(type: string, message: string) {
+    var alertPlaceholder = document.getElementById(
+      'liveAlertPlaceholder'
+    ) as HTMLElement;
+
+    var wrapper = document.createElement('div');
+    wrapper.innerHTML =
+      '<div class="alert alert-' +
+      type +
+      ' alert-dismissible" role="alert">' +
+      message +
+      '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+
+    alertPlaceholder.append(wrapper);
+
+    setTimeout(() => {
+      alertPlaceholder.innerHTML = '';
+    }, 10000);
+  }
+}
